54958793f845f8ba60952e06f33cdb0696fc123c
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-06-13 00:41:10 +0200

    createwebhook

---
+++ commands/createWebhook.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name: "createWebhook",
+    code: `https://discord.com/api/webhooks/$createWebhook[$channelID;$message;https://cdn.discordapp.com/attachments/820367515350728727/842788581584142366/1200px-Discord-Logo-Color.png;yes;/]`
+}
