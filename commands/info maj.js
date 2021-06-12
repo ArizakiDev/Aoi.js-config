b3f713c6b76591d35218a75815a2b32c79f97f7a
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-06-12 11:58:06 +0200

    info maj.js

---
+++ commands/info maj.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "info-maj",
+  code: `$onlyForIDs[820360564219707392;:x: vous n'êtes pas le dev]
+  
+  $title[**Mise à jour]
+  $description[> version V1.1
+
+- Fix beug avec les commande et l'hebergeur
+
+- Ajout de commande premium
+
+- Ajout de commande devloppeur
+
+- Ajout d'un meilleur style niveaux présentation et embed]
+$footer[Xipho Mise à jour]`
+}
