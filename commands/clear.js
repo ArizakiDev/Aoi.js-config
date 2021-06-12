7929f2e973e6421cf1f743bf6b0bd450198bd620
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-06-13 00:41:24 +0200

    clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,4 +1,4 @@
 module.exports = {
     name: "clear",
-    code: `$clear[$message] $message messages effacer`
+    code: `$clear[$message] $title[**CLEAR**] $description[$message messages ont été éffacer] $deleteIn[2s]`
 }
