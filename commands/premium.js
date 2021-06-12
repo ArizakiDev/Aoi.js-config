0f34f65da8a8f570fd2d928e38b3c5e86f781d6d
Oreki ASTRAL <79724270+Oreki-ASTRAL@users.noreply.github.com>
2021-06-12 10:54:48 +0200

    Delete premium.js

--- commands/premium.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    name: "premium",
-    code: `$title[Premium] $description[veuillez faire \`%add-premium\` pour envoyer un message au dévloppeur pour dire que vous désirez avoir le premium]`
-}
